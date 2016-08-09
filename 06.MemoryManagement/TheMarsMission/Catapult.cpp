4f72ac36039c44992c71ffe22227a76a33d3ce3a
M-Yankov <pointblank12345@abv.bg>
2016-08-10 00:17:53 +0300

    Catapult uses random generator for getting the cell for strike.

--- 06.MemoryManagement/TheMarsMission/Catapult.cpp
+++ 06.MemoryManagement/TheMarsMission/Catapult.cpp
@@ -1,13 +1,29 @@
 #include "Catapult.h"
 
-Catapult::Catapult() : Catapult(this->defaultChar)
+Catapult::Catapult() : Catapult(this->defaultChar, RandomGenerator())
 {
 }
 
-Catapult::Catapult(char initalSymbol) : GameElement(initalSymbol)
+Catapult::Catapult(char initalSymbol , RandomGenerator & generator) : GameElement(initalSymbol), randomGenerator(generator)
 {
 }
 
 Catapult::~Catapult()
 {
+}
+
+Cell Catapult::getCellToStrike(int maxRow, int maxColumn)
+{
+    int row = this->randomGenerator.getRandomNumber(0, maxRow);
+    int column = this->randomGenerator.getRandomNumber(0, maxColumn);
+
+    return Cell(row, column);
+}
+
+Cell Catapult::getCellToStrike(int minRow, int maxRow, int minColumn, int maxColumn)
+{
+    int row = this->randomGenerator.getRandomNumber(minRow, maxRow);
+    int column = this->randomGenerator.getRandomNumber(minColumn, maxColumn);
+
+    return Cell(row, column);
 }
--- 06.MemoryManagement/TheMarsMission/Catapult.h
+++ 06.MemoryManagement/TheMarsMission/Catapult.h
@@ -2,15 +2,20 @@
 #define CATAPULT_h
 
 #include "GameElement.h"
-class Catapult :
-    public GameElement
+#include "RandomGenerator.h"
+#include "Cell.h"
+
+class Catapult : public GameElement
 {
 private:
     static const char defaultChar = '<';
+    RandomGenerator randomGenerator;
 public:
     Catapult();
-    Catapult(char initalSymbol);
+    Catapult(char initalSymbol, RandomGenerator & generator);
     ~Catapult();
+    Cell getCellToStrike(int maxRow, int maxColumn);
+    Cell getCellToStrike(int minRow, int maxRow, int minColumn, int maxColumn);
 };
 
 #endif // !CATAPULT_h
